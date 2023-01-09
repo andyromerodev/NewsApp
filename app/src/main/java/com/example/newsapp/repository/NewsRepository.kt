0baa3877817ea934d7ca640043a65b6637d83b7b
andyromero-algo <andyromero.algo@gmail.com>
2023-01-09 11:13:57 -0500

    Injecting NewsProvider and querying to the endpoint

--- app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
@@ -1,18 +1,39 @@
 package com.example.newsapp.repository
 
 import com.example.newsapp.model.News
+import com.example.newsapp.provider.NewsProvider
+import javax.inject.Inject
 
 interface NewsRepository {
     suspend fun getNews(country: String): List<News>
     fun getNew(title: String): News
 }
 
-class NewsRepositoryImp: NewsRepository {
+class NewsRepositoryImp @Inject constructor(
+    private val newsProvider: NewsProvider // Injecting NewsProvider
+): NewsRepository {
+
+    private var news: List<News> = emptyList()
+
     override suspend fun getNews(country: String): List<News> {
-        TODO("Not yet implemented")
+        val apiResponse = newsProvider.topHeadLines(country).body() // Querying to the endpoint
+        if (apiResponse?.status == "error"){// checking status
+            when(apiResponse.code){// checking apiKey
+                "apiKeyMissing" -> throw MissingApiKeyException()
+                "apiKeyInvalid" -> throw InvalidApiKeyException()
+                else -> throw Exception()
+            }
+        }
+
+        news = apiResponse?.articles?: emptyList()
+        return news
+
     }
 
-    override fun getNew(title: String): News {
-        TODO("Not yet implemented")
-    }
+    override fun getNew(title: String): News =
+        news.first{it.title == title}
+
 }
+
+class MissingApiKeyException: java.lang.Exception()
+class InvalidApiKeyException: java.lang.Exception()
